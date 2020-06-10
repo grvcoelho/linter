72325f78cd7b790c361d8573d28a9e484f24af68
Guilherme Coelho <guilhermervcoelho@gmail.com>
2020-06-10 16:36:58 -0300

    react: add first configuration

---
+++ packages/eslint-config-grvcoelho-react/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'grvcoelho-vanilla',
+  ],
+  plugins: [
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  rules: {
+    'jsx-a11y/label-has-for': ['off'],
+    'react/jsx-filename-extension': ['off'],
+    'react/jsx-one-expression-per-line': ['off'],
+    'react-hooks/exhaustive-deps': ['warn'],
+    'react-hooks/rules-of-hooks': ['error'],
+  },
+}
